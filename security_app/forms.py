aacb5170a5a53aec66847c9a66eff23c078d2106
SheikhRokon <mdrokon.u545@gmail.com>
2023-02-26 18:29:42 +0600

    yes

--- security_app/forms.py
+++ security_app/forms.py
@@ -2,13 +2,11 @@ from .models import *
 from django import forms
 
 subjects = (
-        
-        ('django Web Application', 'Service-1'),    
-        ('django Web Application', 'Service-2'),
     
     )
 class Contactfrom (forms.ModelForm):
-    
+    field1 = forms.ModelChoiceField(queryset=Service_Section.objects.all(),empty_label="Choose any Service")
+
     full_name = forms.CharField(widget= forms.TextInput(attrs={
         'class': 'form-control',
         'placeholder' : "Your Full Name" }))
@@ -38,41 +36,8 @@ class Contactfrom (forms.ModelForm):
 
     class Meta:
         model = Contat
-        fields = '__all__'
-
-class Contactfrom (forms.ModelForm):
-    
-    full_name = forms.CharField(widget= forms.TextInput(attrs={
-        'class': 'form-control',
-        'placeholder' : "Your Full Name" }))
-
-    email = forms.EmailField(widget= forms.EmailInput(attrs ={
-        'class': 'form-control', 'placeholder':"Your Email" }))
-
-    phone = forms.CharField(widget= forms.TextInput(attrs={
-        'class': 'form-control',
-        'placeholder' : "Phone Number"}))
-
-    company = forms.CharField(widget= forms.TextInput(attrs={
-        'class': 'form-control',
-        'placeholder' : "Your Company" }))
-    
-    satet = forms.CharField(widget= forms.TextInput(attrs={
-        'class': 'form-control',
-        'placeholder' : "Your Satet" }))
-
-    topics = forms.ChoiceField(choices=subjects,widget=forms.Select(attrs={
-        'class': 'form-select' }))
-
-    massege = forms.CharField(widget= forms.Textarea(attrs={
-        'class': 'form-control','placeholder' :"Your Comment" , 'row': 3 }))
-    
-
-
-    class Meta:
-        model = Contat
-        fields = '__all__'        
-
+        fields = ['field1','full_name']
+       
     
 
 
